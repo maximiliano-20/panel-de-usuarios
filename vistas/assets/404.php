803267a8e765d50023535c2e9227e4698a13cc66
maximiliano-20 <estigarribiamaximiliano73@gmail.com>
2022-03-31 17:29:25 -0300

    Vista 404

---
+++ vistas/assets/404.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 3 | 404 Page not found</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel="stylesheet" href="<?=url?>vistas/modulos/dist/css/adminlte.css">
+  <link rel="stylesheet" href="<?=url?>vistas/modulos/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+</head>
+<body class="hold-transition sidebar-mini">
+
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>404 Pagina No Encontrada</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item active">404 Pagina No Encontrada</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="error-page">
+        <h2 class="headline text-warning"> 404</h2>
+
+        <div class="error-content">
+          <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops! No Pudimos Encontrar Tu Pagina.</h3>
+
+          <p>
+            Puedes contactar con el administrador del sitio 
+          </p>
+
+         
+        </div>
+        <!-- /.error-content -->
+      </div>
+      <!-- /.error-page -->
+    </section>
+    <!-- /.content -->
+
+
+
+</body>
+</html>
